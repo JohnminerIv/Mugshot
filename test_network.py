f1395854b2775bd3e47478f1ce21fbce8eca9af9
John Miner <john.douglas.miner.iv@gmail.com>
2020-01-10 13:23:46 -0800

    changing format

--- test_network.py
+++ test_network.py
@@ -5,38 +5,43 @@ import argparse
 import imutils
 import cv2
 
-# construct the argument parse and parse the arguments
-ap = argparse.ArgumentParser()
-ap.add_argument("-m", "--model", required=True, help="path to trained model model")
-ap.add_argument("-i", "--image", required=True, help="path to input image")
-args = vars(ap.parse_args())
 
-# load the image
-image = cv2.imread(args["image"])
-orig = image.copy()
+def main(model, image):
+    # load the image
+    image = cv2.imread(image)
+    orig = image.copy()
 
-# pre-process the image for classification
-image = cv2.resize(image, (56, 56))
-image = image.astype("float") / 255.0
-image = img_to_array(image)
-image = np.expand_dims(image, axis=0)
+    # pre-process the image for classification
+    image = cv2.resize(image, (56, 56))
+    image = image.astype("float") / 255.0
+    image = img_to_array(image)
+    image = np.expand_dims(image, axis=0)
 
-# load the trained convolutional neural network
-print("[INFO] loading network...")
-model = load_model(args["model"])
+    # load the trained convolutional neural network
+    print("[INFO] loading network...")
+    model = load_model(model)
 
-# classify the input image
-(notmug, mug) = model.predict(image)[0]
+    # classify the input image
+    (notmug, mug) = model.predict(image)[0]
 
-# build the label
-label = "Mug" if mug > notmug else "Not Mug"
-proba = mug if mug > notmug else notmug
-label = "{}: {:.2f}%".format(label, proba * 100)
+    # build the label
+    label = "Mug" if mug > notmug else "Not Mug"
+    proba = mug if mug > notmug else notmug
+    label = "{}: {:.2f}%".format(label, proba * 100)
 
-# draw the label on the image
-output = imutils.resize(orig, width=400)
-cv2.putText(output, label, (10, 25),  cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 255, 0), 2)
+    # draw the label on the image
+    output = imutils.resize(orig, width=400)
+    cv2.putText(output, label, (10, 25),  cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 255, 0), 2)
 
-# show the output image
-cv2.imshow("Output", output)
-cv2.waitKey(0)
+    # show the output image
+    cv2.imshow("Output", output)
+    cv2.waitKey(0)
+
+
+if __name__ == '__main__':
+    # construct the argument parse and parse the arguments
+    ap = argparse.ArgumentParser()
+    ap.add_argument("-m", "--model", required=True, help="path to trained model model")
+    ap.add_argument("-i", "--image", required=True, help="path to input image")
+    args = vars(ap.parse_args())
+    main(args["model"], args["image"])
